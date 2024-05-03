1985f1f439bb9205fe867da894bd1c472b1fe2f6
Shutov Dmitrii <fplatoff@gmail.com>
2024-05-03 15:44:55 +0400

    Step 8/14

--- game/main.py
+++ game/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import curses
+import random
 import time
 
 
@@ -25,9 +26,19 @@ async def blink(canvas, row, column, symbol='*'):
 def draw(canvas: curses.window):
     curses.curs_set(False)
     canvas.border()
+    size = canvas.getmaxyx()
+    symbols = '+*.:'
+    # canvas.addstr(random.randint(1, size[0]), random.randint(1, size[1]), str(size))
+    # canvas.refresh()
+    # time.sleep(10)
 
-    coroutines = [blink(canvas, 5, (i + 1) * 5) for i in range(5)]
+    coroutines = [blink(canvas,
+                        random.randint(1, size[0] - 2),
+                        random.randint(1, size[1] - 2),
+                        symbol=random.choice(symbols)) for _ in range(500)]
 
+    # coroutines = [blink(canvas, 5, (i + 1) * 5) for i in range(10)]
+    #
     while True:
         for coroutine in coroutines:
             coroutine.send(None)
