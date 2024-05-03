d8ecab3cf4c407162367ea8efc94d871deb70437
Shutov Dmitrii <fplatoff@gmail.com>
2024-05-03 18:09:11 +0400

    Step 10/14

--- game/main.py
+++ game/main.py
@@ -23,22 +23,62 @@ async def blink(canvas, row, column, symbol='*', max_delay=4):
             await asyncio.sleep(0)
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 def draw(canvas: curses.window):
     curses.curs_set(False)
     canvas.border()
     size = canvas.getmaxyx()
+    row_max_canvas, column_max_canvas = size[0] - 2, size[1] - 2
     symbols = '+*.:'
 
     coroutines = [blink(canvas,
-                        random.randint(1, size[0] - 2),
-                        random.randint(1, size[1] - 2),
-                        symbol=random.choice(symbols)) for _ in range(500)]
+                        random.randint(1, row_max_canvas),
+                        random.randint(1, column_max_canvas),
+                        symbol=random.choice(symbols)) for _ in range(100)]
+
+    coroutines.append(fire(canvas,
+                           row_max_canvas,
+                           10,
+                           ))
 
     while True:
-        for coroutine in coroutines:
-            coroutine.send(None)
-        canvas.refresh()
-        time.sleep(0.1)
+        try:
+            for coroutine in coroutines:
+                coroutine.send(None)
+            canvas.refresh()
+            time.sleep(0.1)
+        except StopIteration:
+            break
+        time.sleep(10)
 
 
 if __name__ == '__main__':
